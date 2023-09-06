04fb6429bbfc17cd356f41b2c78a989d0c0644a2
Adivya Jain <95540661+adivya-jain@users.noreply.github.com>
2023-09-06 18:43:42 +0530

    updated Home.component.js and app.js files
    added dropdown option to LeadStatus column

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import HomeComponent from "./home.component";
-
+// import HomeComponent2 from "./home2demo";
 function App() {
   return (
     <main> 
         <HomeComponent /> 
+    {/* <HomeComponent2/> */}
     </main>
   );
 }
